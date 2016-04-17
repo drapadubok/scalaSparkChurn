da9caebc0f8757dbbcb8b4b6d2846437065fa3f2
Dmitry Smirnov <dmi.smirnov07@gmail.com>
2016-04-17 11:08:33 +0300

    Update ChurnData.scala

--- src/main/scala/churn/ChurnData.scala
+++ src/main/scala/churn/ChurnData.scala
@@ -2,51 +2,63 @@ package churn
 
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.util.MLUtils.saveAsLibSVMFile
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
+
 
 object ChurnData extends App {
   // Setup local standalone spark, 4 workers I guess?
   //val conf = new SparkConf().setMaster("local[4]").setAppName("Churner")
-  val conf = new SparkConf().setAppName("Churn")
+  val conf = new SparkConf().setAppName("ChurnData")
   val sc = new SparkContext(conf)
+  val sqlContext = new SQLContext(sc)
+  import sqlContext.implicits._
 
   def bool2int(b:Boolean): Int = if (b) 1 else 0
 
-  def getDropColumns(head: Array[String], nameToDrop: Array[String]) = {
-    // Given column names, get the indices of these columns
-    nameToDrop.map(lab => head.indexWhere(_==lab))
+  def getIdxDropColumns(head: Array[String], columnsToDrop: Array[String]) = {
+    columnsToDrop.map(lab => head.indexWhere(_==lab))
   }
 
-  def parseLine(line: Array[String], colsToDrop: Array[Int]) = {
+  def dropColumns(line: Array[String], indices: Array[Int]) = {
+    // To drop columns, zip with index, filter columns that match indices
+    line.zipWithIndex.filterNot( x => indices.contains(x._2) ).map(_._1)
+  }
+
+  def parseLine(line: Array[String]): LabeledPoint = {
     val label = bool2int(line.last.contains("True"))
-    // Drop columns, zip with index, filter columns that match indices in colsToDrop
-    val droppedCols = line.zipWithIndex.filterNot( x => colsToDrop.contains(x._2) ).map(_._1)
-    // Drop label
-    val piecesNoLabel = droppedCols.take(droppedCols.length-1)
+    val noLabel = line.init
     // Convert y/n to bool
-    val piecesFin = piecesNoLabel.map {
-      case "yes" => 1.0//true
-      case "no" => 0.0//false
+    val features = noLabel.map {
+      case "yes" => 1.0
+      case "no" => 0.0
       case other => other.toDouble
     }
-    LabeledPoint(label, Vectors.dense(piecesFin))
+    LabeledPoint(label, Vectors.dense(features))
   }
 
   def parseData (url: String) = {
     val text = sc.textFile(url)
     val rdd = text.map(_.split(","))
     val head = rdd.first
-    val body = rdd.filter(_(0) != head(0))
 
-    val nameToDrop = Array("Phone","Area Code","State")
-    val colsToDrop = getDropColumns(head, nameToDrop)
+    // Drop columns that contain useless info
+    val columnsToDrop = Array("Phone","Area Code","State")
+    val idxColumnsToDrop = getIdxDropColumns(head, columnsToDrop)
+    val rddColumnsDropped = rdd.map(dropColumns(_, idxColumnsToDrop))
 
-    body.map(parseLine(_, colsToDrop))
+    // Split into header and data
+    val header = rddColumnsDropped.first
+    val body = rddColumnsDropped.filter(_(0) != header(0))
+
+    // Parse data, keep header just in case, might use it later
+    (header, body.map(parseLine))
   }
 
   val output = parseData("s3n://dimaspark/churn_data.csv")
-  output.saveAsTextFile("s3n://dimaspark/churn_processed/")
-
+  saveAsLibSVMFile(output._2, "s3n://dimaspark/churn_processed_libsvm/")
+  //val df = output._2.toDF
+  //df.write.mode("overwrite").save("s3n://dimaspark/churn_processed_libsvm/")
   sc.stop()
 }
-
